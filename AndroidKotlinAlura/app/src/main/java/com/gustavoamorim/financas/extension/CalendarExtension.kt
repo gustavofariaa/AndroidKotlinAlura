29a9bfb63a3d70844dbfbc13d4a43e2deca6df7d
Gustavo Amorim | Gerencianet <gustavo.amorim@gerencianet.com.br>
2021-03-13 00:45:24 -0300

    Kotlin parte 1: Sua primeira app android na linguagem

--- AndroidKotlinAlura/app/src/main/java/com/gustavoamorim/financas/extension/CalendarExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.gustavoamorim.financas.extension
-
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
-fun Calendar.brazilianFormat(): String {
-    val brazilianFormat = SimpleDateFormat("dd/MM/yyyy")
-    return brazilianFormat.format(this.time)
-}
--- AndroidKotlinAlura/app/src/main/java/com/gustavoamorim/financas/extension/StringExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.gustavoamorim.financas.extension
-
-fun String.limitCharacters(stringLength: Int) : String {
-    if (this.length > stringLength) {
-        val initialPosition = 0
-        return "${this.substring(initialPosition, stringLength)}..."
-    }
-    return this
-}
